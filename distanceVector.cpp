e3033103b6ea5718252c10aca99d68205b310e3e
adrianhp97 <13515091@std.stei.itb.ac.id>
2017-11-17 21:55:08 +0700

    edit pushVector

--- distanceVector.cpp
+++ distanceVector.cpp
@@ -7,12 +7,12 @@ using namespace std;
 vector<vector<int> > inputNode();
 vector<vector<int> > inputSkenario();
 vector<int> convertStringToInt(string text);
+vector<int> pushVector(int idx, string text);
 void printVector(vector<vector<int> > vectorPrint);
 
 int main() {
 	vector<vector<int> > neighbour_table(inputNode());
 	vector<vector<int> > skenario_table(inputSkenario());
-
 	return 0;
 }
 
@@ -23,9 +23,11 @@ vector<vector<int> > inputNode() {
 	cin >> n_node;
 	for (int idx = 0; idx < n_node; idx++) {
 		string neighbour_node;
+		vector<int> single_node;
 
 		cin >> neighbour_node;
-		node.push_back(convertStringToInt(neighbour_node));
+		single_node = pushVector(idx, neighbour_node);
+		node.push_back(single_node);
 	}
 	return node;
 }
@@ -37,9 +39,11 @@ vector<vector<int> > inputSkenario() {
 	cin >> n_skenario;
 	for (int idx = 0; idx < n_skenario; idx++) {
 		string skenario_text;
+		vector<int> single_skenario;
 
 		cin >> skenario_text;
-		skenario.push_back(convertStringToInt(skenario_text));
+		single_skenario = pushVector(idx, skenario_text);
+		skenario.push_back(single_skenario);
 	}
 	return skenario;
 }
@@ -54,10 +58,28 @@ vector<int> convertStringToInt(string text) {
 	return arrayOfInt;
 }
 
+vector<int> pushVector(int idx, string text) {
+	vector<int> pushedVector;
+
+	if(text.compare("\n") != 82) {
+		vector<int>::iterator it;
+
+		pushedVector = convertStringToInt(text);
+		it = pushedVector.begin();
+		pushedVector.insert(it, idx+1);
+	} else {
+		pushedVector.push_back(idx+1);
+	}
+	
+	return pushedVector;
+}
+
 void printVector(vector<vector<int> > vectorPrint) {
 	for(vector<vector<int> >::iterator iit = vectorPrint.begin(); iit != vectorPrint.end(); iit++) {
-		for(vector<int>::iterator jit = (*iit).begin(); jit != (*iit).end(); jit++) {
-			cout << *jit << ' ';
+		if((*iit).size() != 0) {
+			for(vector<int>::iterator jit = (*iit).begin(); jit != (*iit).end(); jit++) {
+				cout << *jit << ' ';
+			}
 		}
 		cout << endl;		
 	}
